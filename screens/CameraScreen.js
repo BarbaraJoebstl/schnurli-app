57259f53e182fed86d90a3ffb5c3df2d55ec56b1
Barbara <babsi.joebstl@gmail.com>
2018-11-13 20:19:19 +0100

    add timestamp for mailings, toggle af

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import { Camera, Permissions, MailComposer } from 'expo';
 
 export default class CameraScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
+  //TODO enum for white balance Camera.Constants.WhiteBalance: auto, sunny, cloudy, shadow, fluorescent, incandescent
+
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
+    af: true,
     path: null
   };
 
@@ -21,8 +24,21 @@ export default class CameraScreen extends React.Component {
   };
 
   send = () => {
-    alert('TODO: impl. send email')
+    const readable_timestamp = new Date().toLocaleString();
+
+    MailComposer.composeAsync({
+      // todo 
+      // - get email recipients
+      // - subject
+      // - body text via settings
+      recipients: [],
+      subject: `You schnurlizerd: ${readable_timestamp}`,
+      body: "Hi! Here is the attachment...",
+      attachments: [`${this.state.path}`]
+    })
+
     this.setState({ path: null });
+
   }
 
   async componentWillMount() {
@@ -32,54 +48,71 @@ export default class CameraScreen extends React.Component {
 
   renderCamera() {
     return (
-        <Camera style={{ flex: 1 }}
-          type={this.state.type}
-          ref={ref => { this.camera = ref; }}>
-          <View
+      <Camera style={{ flex: 1 }}
+        type={this.state.type}
+        ref={ref => { this.camera = ref; }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
             style={{
-              flex: 1,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
+              alignSelf: 'center',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              this.schnurliwurli()
             }}>
-            <TouchableOpacity
-              style={{
-                alignSelf: 'center',
-                alignItems: 'center',
-              }}
-              onPress={() => {
-                this.schnurliwurli()
-              }}>
-              <Text
-                style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                {' '}Shot{' '}
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
+            <Text
+              style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+              {' '}Shot{' '}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
             style={{
-              flex: 1,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              this.setState({
+                type: this.state.type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back,
+              });
             }}>
-            <TouchableOpacity
-              style={{
-                alignSelf: 'flex-end',
-                alignItems: 'center',
-              }}
-              onPress={() => {
-                this.setState({
-                  type: this.state.type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back,
-                });
-              }}>
-              <Text
-                style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                {' '}Flip{' '}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </Camera>
+            <Text
+              style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+              {' '}Flip{' '}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              this.setState({
+                type: this.state.af === true
+                  ? Camera.Constants.AutoFocus = true
+                  : Camera.Constants.AutoFocus = false,
+              });
+            }}>
+            <Text
+              style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+              {' '}Toggle AF{' '}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     );
   }
 
@@ -88,7 +121,8 @@ export default class CameraScreen extends React.Component {
       <View style={{ flex: 1 }}>
         <Image
           source={{ uri: this.state.path }}
-          style={{  flex: 1,
+          style={{
+            flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
             height: 100,
@@ -96,18 +130,18 @@ export default class CameraScreen extends React.Component {
           }}
         />
         <Text
-         style={{
-          alignSelf: 'flex-end',
-          alignItems: 'center',
-        }}
+          style={{
+            alignSelf: 'flex-end',
+            alignItems: 'center',
+          }}
           onPress={() => this.setState({ path: null })}
         >Cancel
           </Text>
         <Text
-         style={{
-          alignSelf: 'flex-end',
-          alignItems: 'center',
-        }}
+          style={{
+            alignSelf: 'flex-end',
+            alignItems: 'center',
+          }}
           onPress={() => { this.send() }}>
           Send
         </Text>
@@ -122,10 +156,10 @@ export default class CameraScreen extends React.Component {
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     } else {
-      return(
-      <View style={{ flex: 1 }}>
+      return (
+        <View style={{ flex: 1 }}>
           {this.state.path ? this.renderPreview() : this.renderCamera()}
-      </View>
+        </View>
       )
     }
   }
